d55e602b2af985f4d93c8acf88ea761289ab9feb
sandeep <sandeepkumarpani888@gmail.com>
2021-06-07 05:15:07 +0100

    changes for working in collab

--- neural_renderer/look_at.py
+++ neural_renderer/look_at.py
@@ -1,7 +1,64 @@
 import chainer
 import chainer.functions as cf
 
-import neural_renderer
+import cupy as cp
+import numpy as np
+
+
+class Cross(chainer.Function):
+    def check_type_forward(self, in_types):
+        chainer.utils.type_check.expect(
+            in_types[0].dtype.kind == 'f',
+            in_types[0].ndim == 2,
+            in_types[0].shape[1] == 3,
+            in_types[1].dtype.kind == 'f',
+            in_types[1].ndim == 2,
+            in_types[1].shape[1] == 3,
+            in_types[0].shape[0] == in_types[1].shape[0],
+        )
+
+    def forward_cpu(self, inputs):
+        a, b = inputs
+        c = np.cross(a, b)
+        return c,
+
+    def forward_gpu(self, inputs):
+        a, b = inputs
+        c = cp.zeros_like(a, 'float32')
+        chainer.cuda.elementwise(
+            'int32 j, raw T a, raw T b',
+            'raw T c',
+            '''
+                float* ap = (float*)&a[j * 3];
+                float* bp = (float*)&b[j * 3];
+                float* cp = (float*)&c[j * 3];
+                cp[0] = ap[1] * bp[2] - ap[2] * bp[1];
+                cp[1] = ap[2] * bp[0] - ap[0] * bp[2];
+                cp[2] = ap[0] * bp[1] - ap[1] * bp[0];
+            ''',
+            'function',
+        )(
+            cp.arange(a.size / 3).astype('int32'), a, b, c,
+        )
+        return c,
+
+    def backward_cpu(self, inputs, gradients):
+        a, b = inputs
+        gc = gradients[0]
+        ga = np.cross(b, gc)
+        gb = np.cross(gc, a)
+        return ga, gb
+
+    def backward_gpu(self, inputs, gradients):
+        a, b = inputs
+        gc = gradients[0]
+        ga = self.forward_gpu((b, gc))[0]
+        gb = self.forward_gpu((gc, a))[0]
+        return ga, gb
+
+
+def cross(a, b):
+    return Cross()(a, b)
 
 
 def look_at(vertices, eye, at=None, up=None):
@@ -28,8 +85,8 @@ def look_at(vertices, eye, at=None, up=None):
 
     # create new axes
     z_axis = cf.normalize(at - eye)
-    x_axis = cf.normalize(neural_renderer.cross(up, z_axis))
-    y_axis = cf.normalize(neural_renderer.cross(z_axis, x_axis))
+    x_axis = cf.normalize(cross(up, z_axis))
+    y_axis = cf.normalize(cross(z_axis, x_axis))
 
     # create rotation matrix: [bs, 3, 3]
     r = cf.concat((x_axis[:, None, :], y_axis[:, None, :], z_axis[:, None, :]), axis=1)
